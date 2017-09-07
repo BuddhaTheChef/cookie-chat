9822a946216835b16d2e6fbbff451f2a927a8457
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-09-07 11:38:19 -0500

    cookie chay fin

--- js/messageService.js
+++ js/messageService.js
@@ -1,5 +1,22 @@
 angular.module('chatroom').service('messageService', function($http){
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
+var baseurl = 'https://practiceapi.devmountain.com/api/chats';
+
+ this.getMessages = function(){
+
+    return $http.get(baseurl).then(function(response){
+      // console.log(response)
+      return response.data
+    })
+
+ };
+
+this.postMessage = function(message){
+
+return $http.post(baseurl,{message:message })
+
+
+};
 
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'https://practiceapi.devmountain.com/api/chats'
